449841d45dcdec6d2d88b6b7f231b4752d2c4e77
Gilad Naaman <gilad@levltech.com>
2020-03-16 19:07:13 +0200

    Switch to autocfg from rustc_version

--- build.rs
+++ build.rs
@@ -1,20 +1,14 @@
-extern crate rustc_version;
-use rustc_version::{version, Version};
-
 fn main() {
-    let version = version().unwrap();
-
-    // Assert we haven't travelled back in time
-    assert!(version.major >= 1);
+    let ac = autocfg::new();
 
     // Check for a minimum version for a few features
-    if version >= Version::from((1, 31, 0)) {
+    if ac.probe_rustc_version(1, 31) {
         println!("cargo:rustc-cfg=allow_clippy");
     }
-    if version >= Version::from((1, 36, 0)) {
+    if ac.probe_rustc_version(1, 36) {
         println!("cargo:rustc-cfg=maybe_uninit");
     }
-    if version >= Version::from((1, 40, 0)) {
+    if ac.probe_rustc_version(1, 40) {
         println!("cargo:rustc-cfg=doctests");
     }
 }
