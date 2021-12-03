42483952ce1913f56d955e920c63ff5f3843d5d3
yvt <i@yvt.jp>
2021-12-03 22:22:23 +0900

    Remove `#![feature(const_raw_ptr_deref)]`
    It's now stable in nightly.

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@
 #![no_std]
 #![cfg_attr(
     feature = "unstable_const",
-    feature(const_ptr_offset_from, const_raw_ptr_deref, const_refs_to_cell)
+    feature(const_ptr_offset_from, const_refs_to_cell)
 )]
 
 #[macro_use]
